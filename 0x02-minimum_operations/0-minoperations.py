afa2651df00de5c5ac4a5476482337dc2a3c1fef
Code021-raelle <gabrielakinshola3@gmail.com>
2024-07-11 18:00:10 +0100

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module to calculate the minimum number of operations
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations needed to result
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
